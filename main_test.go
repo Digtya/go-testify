f6ccce3ec269747a2794537c47c740a4e63ef252
Ivan D <digtya@mail.ru>
2024-08-20 23:46:47 +0500

    iteration 3

--- main_test.go
+++ main_test.go
@@ -28,9 +28,7 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status == http.StatusOK {
-		t.Fatalf("expected status code: %d, got %d", http.StatusBadRequest, status)
-	}
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 
 	body := responseRecorder.Body.String()
 	assert.Contains(t, body, "wrong city value")
@@ -41,9 +39,10 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
 
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
